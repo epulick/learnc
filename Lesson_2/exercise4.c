d70e571ed7ec7f2ee9dd756d77d5fd2d8f37d85f
emp <ericmpul@umich.edu>
2018-06-12 23:14:11 +0200

    6/12 -- The programmers have not realized that I am a mechanical engineer yet. Day 3, journey to be continued.

---
+++ Lesson_2/exercise4.c
@@ -0,0 +1,14 @@
+// ex02-4.c
+
+#include <stdio.h>
+
+int main(void)
+{
+    int ctr;
+
+    for(ctr = 65; ctr < 91; ctr++)
+        printf("%c", ctr);
+
+    return 0;
+
+}
--- type_run_1/tr1.c
+++ type_run_1/tr1.c
@@ -5,7 +5,7 @@
 
 void do_heading(char *filename);
 
-int line = 0; page = 0;
+int line = 0, page = 0;
 
 int main(int argv, char *argc[])
 {
